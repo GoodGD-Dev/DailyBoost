43082de360fcf79f0c938100b644c390c4b6e6fd
goodgd <dionatha.work@gmail.com>
2025-05-16 00:34:16 -0300

    first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      bootstrap: path.resolve(__dirname, 'node_modules/bootstrap')
+    }
+  }
+})
